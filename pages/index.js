ba2f42e5ac2f1336858efc92ddb6dcd5d969b0eb
Julian Cantera <julian.c.dev@gmail.com>
2022-07-13 09:52:41 +0100

    Modify Product.jsx

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,9 @@ const Home = ({ products, bannerData }) => {
         <p>Speaker of many variations</p>
       </div>
       <div className="products-container">
-        {products?.map((product) => product.name)}
+        {products?.map((product) => (
+          <Product key={product._id} product={product} />
+        ))}
       </div>
       Footer
       <FooterBanner />
